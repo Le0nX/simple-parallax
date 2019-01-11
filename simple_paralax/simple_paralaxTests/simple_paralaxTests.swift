a4add915cfecc17efa641c2ace4d3cd6c498fac4
Le0nX <cloudmindone@gmail.com>
2019-01-11 19:49:15 +0300

    feature: added TableViewController & images

---
+++ simple_paralax/simple_paralaxTests/simple_paralaxTests.swift
@@ -0,0 +1,34 @@
+//
+//  simple_paralaxTests.swift
+//  simple_paralaxTests
+//
+//  Created by Denis Nefedov on 11/01/2019.
+//  Copyright © 2019 X. All rights reserved.
+//
+
+import XCTest
+@testable import simple_paralax
+
+class simple_paralaxTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
