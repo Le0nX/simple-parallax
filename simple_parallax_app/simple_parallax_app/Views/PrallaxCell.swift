166429eed372ddd277cbc73a5235daed18db43a9
Le0nX <cloudmindone@gmail.com>
2019-01-11 22:15:32 +0300

    release: version v.1.0

--- simple_parallax_app/simple_parallax_app/Views/PrallaxCell.swift
+++ simple_parallax_app/simple_parallax_app/Views/PrallaxCell.swift
@@ -20,12 +20,31 @@ class PrallaxCell: UITableViewCell {
     }
     
     func setupParallax() {
+        let min = CGFloat(-30)
+        let max = CGFloat(30)
         
+        // effect one for x axis
+        let xMotion = UIInterpolatingMotionEffect(keyPath: "layer.transform.translation.x", type: .tiltAlongHorizontalAxis)
+        xMotion.minimumRelativeValue = min
+        xMotion.maximumRelativeValue = max
+        
+        //effect two for y axis
+        let yMotion = UIInterpolatingMotionEffect(keyPath: "layer.transform.translation.x", type: .tiltAlongVerticalAxis)
+        yMotion.minimumRelativeValue = min
+        yMotion.maximumRelativeValue = max
+        
+        // creating group for our two effects
+        let motionEffectGroup = UIMotionEffectGroup()
+        motionEffectGroup.motionEffects = [xMotion, yMotion]
+        
+        // adding effects to our ImageView
+        cellImage.addMotionEffect(motionEffectGroup)
     }
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code setupParallax
+        setupParallax()
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
